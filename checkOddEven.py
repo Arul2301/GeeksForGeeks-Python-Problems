b10039c704b075832e22dc68a7345bdcc31c4c25
Arul2301 <71940931+Arul2301@users.noreply.github.com>
2024-11-26 07:07:20 +0530

    Create checkOddEven.py

---
+++ checkOddEven.py
@@ -0,0 +1,26 @@
+def checkOddEven(x):
+    if(x % 2 == 0):
+      return True
+    else:
+      return False
+
+def main():
+    
+    # Testcase input
+    testcases = int(input())
+    
+    # Looping through testcases
+    while(testcases > 0):
+        x = int(input())
+        
+        if(checkOddEven(x)):
+            print ("Even")
+        else:
+            print ("Odd")
+        
+        
+        testcases -= 1
+        print("~")
+ 
+if __name__ == '__main__':
+    main()
